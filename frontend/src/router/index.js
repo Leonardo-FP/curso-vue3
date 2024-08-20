1460dfa9a3bff6937a61a53fe51fe3a95b8bff15
Leonardo Franca Paulelli <90411076+Leonardo-FP@users.noreply.github.com>
2024-08-20 15:17:43 -0300

    lifecycle hooks start

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +8,12 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/products',
+      name: 'products',
+      // Não precisa importar o componente noo topo da página quando é feito com Lazy load
+      component: () => import('../views/ProductsView.vue')
     },
     {
       path: '/about',
