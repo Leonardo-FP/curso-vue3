0e7ff3ff45001b99d417624ab0565eead23e5579
Leonardo Franca Paulelli <90411076+Leonardo-FP@users.noreply.github.com>
2024-09-23 23:05:11 -0300

    propriedade computada

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,73 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <h2>Home</h2>
+  <h2>Full name</h2>
+  {{ fullName }}
+
+  <h2>Admin</h2>
+  <ul>
+    <template v-for="(user, index) in users" :key="index">
+        <li v-if="user.is_admin">{{ user.firstName }}</li>
+    </template>
+  </ul>
+
+  <h2>Not admin</h2>
+  <ul>
+    <template v-for="(user, index) in usersNotAdmin" :key="index">
+        <li>{{ user.firstName }}</li>
+    </template>
+  </ul>
+
+  {{ countComputed }}
+  <button @click="computedCount++">Add computed</button>
+  <br>
+  {{ countFunction() }}
+  <button @click="functionCount++">Add function</button>
 </template>
+
+<script setup>
+  import {computed, reactive, ref} from 'vue';
+
+  const user = reactive({
+    firstName:'Leonardo',
+    lastName:'Paulelli'
+  })
+
+  const computedCount = ref(0);
+  const functionCount = ref(0);
+
+  const users = reactive([
+    {
+      firstName:'Leonardo',
+      is_admin:1
+    },
+    {
+      firstName:'Lorenzzo',
+      is_admin:0
+    },
+    {
+      firstName:'Ana Beatriz',
+      is_admin:0
+    },
+    {
+      firstName:'Lucimara',
+      is_admin:1
+    }
+  ])
+
+  const fullName = computed(() => {
+    return user.firstName + ' ' + user.lastName
+  })
+
+  const usersNotAdmin = computed(() => {
+    return users.filter((user) => user.is_admin === 0);
+  })
+
+  const countComputed = computed(() => {
+    console.log('called computed count');
+    return computedCount.value;
+  })
+
+  function countFunction(){
+    console.log('called function count');
+    return functionCount.value;
+  }
+</script>
