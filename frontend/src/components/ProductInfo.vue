09cac68569d96732e3d82d578fc3f18063656b7f
Leonardo Franca Paulelli <90411076+Leonardo-FP@users.noreply.github.com>
2024-10-10 15:34:16 -0300

    props de pai para filho

---
+++ frontend/src/components/ProductInfo.vue
@@ -0,0 +1,10 @@
+<template>
+    Likes: {{ likes }}
+    Comments: {{ comments }}
+</template>
+
+<script>
+    export default {
+        props:['likes', 'comments']
+    }
+</script>
--- frontend/src/views/ProductsView.vue
+++ frontend/src/views/ProductsView.vue
@@ -1,11 +1,25 @@
 <template>
     <h2 id="app-products">Products</h2>
     <h2>Products my app</h2>
+
+    <h2>Product 1</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam, impedit! Unde molestias a doloribus neque aspernatur voluptates ex expedita dolore, assumenda iste sapiente mollitia praesentium illum veniam itaque maxime? Debitis.</p>
+    <ProductInfo :likes="product.likes" :comments="product.comments" />
 </template>
 
 <script>
+    import ProductInfo from '@/components/ProductInfo.vue';
     export default {
-        name: "Products.vue"
+        components:{ProductInfo},
+        name: "Products.vue",
+        data(){
+            return {
+                product:{
+                    likes:20, 
+                    comments:30
+                }
+            }
+        }
     }
 </script>
 
