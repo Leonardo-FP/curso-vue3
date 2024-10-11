3a05f162550aa61d4ef3c4e0cbb82ee2a4b58714
Leonardo Franca Paulelli <90411076+Leonardo-FP@users.noreply.github.com>
2024-10-10 22:34:45 -0300

    props do pai para o filho

---
+++ frontend/src/components/ProductInfo.vue
@@ -0,0 +1,8 @@
+<template>
+    Likes:{{ likes }}
+    Comments:{{ comments }}
+</template>
+
+<script setup>
+    defineProps(['likes','comments'])
+</script>
--- frontend/src/views/ProductsView.vue
+++ frontend/src/views/ProductsView.vue
@@ -1,12 +1,20 @@
 <template>
     <h2 id="app-products">Products</h2>
     <h2>Products my app</h2>
+
+    <h2>Product 1</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim omnis tempore repellendus optio totam expedita itaque voluptatibus ipsum voluptas beatae dignissimos, cumque excepturi. Facilis placeat minus adipisci est ullam eveniet.</p>
+    <ProductInfo :likes="product.likes" :comments="product.comments" />
 </template>
 
-<script>
-    export default {
-        name: "Products.vue"
-    }
+<script setup>
+    import ProductInfo from '@/components/ProductInfo.vue';
+    import {reactive} from 'vue';
+
+    const product = reactive({
+        likes:20,
+        comments:30,
+    })
 </script>
 
 <style scoped>
