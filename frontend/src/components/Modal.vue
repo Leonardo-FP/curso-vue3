2bb66cf8c5f704bcb8427c47e2294714b4e1b3a6
Leonardo Franca Paulelli <90411076+Leonardo-FP@users.noreply.github.com>
2024-10-11 22:18:13 -0300

    modal com bulma

--- frontend/src/components/Modal.vue
+++ frontend/src/components/Modal.vue
@@ -1,5 +1,24 @@
 <template>
-    <slot name="header"></slot>
-    <slot></slot>
-    <slot name="footer"></slot>
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <div class="box">
+                <slot name="header"></slot>
+                <slot></slot>
+                <slot name="footer"></slot>
+            </div>
+        </div>
+        <button class="modal-close is-large" aria-label="close" @click="close"></button>
+    </div>
 </template>
+
+<script setup>
+    const emit = defineEmits(['close'])
+    function close(){
+        emit('close');
+    }
+</script>
+
+<style scoped>
+    @import "https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css";
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,24 +1,22 @@
 <template>
   <button @click="openModal = true">Open modal</button>
 	<br>
-	<template v-if="openModal">
-		<Modal>
-			<template #header>Header do slot</template>
-			<template #default>
-				<form action="">
-					<input type="text" name="" id="" placeholder="Email">
-					<input type="text" name="" id="" placeholder="Senha">
+	<Modal v-if="openModal" @close="openModal = false">
+		<template #header>Header do modal</template>
+		<template #default>
+			<form action="">
+				<input type="text" name="" id="" placeholder="Email">
+				<input type="text" name="" id="" placeholder="Senha">
 
-					<button type="submit">Submit</button>
-				</form>
-			</template>
-			<template #footer>Footer do slot</template>
-		</Modal>
-	</template>
+				<button type="submit">Submit</button>
+			</form>
+		</template>
+		<template #footer>Footer do modal</template>
+	</Modal>
 </template>
 
 <script setup>
-  import {ref} from 'vue';
+	import {ref} from 'vue';
 	import Modal from '@/components/Modal.vue';
 
 	const openModal = ref(false);
