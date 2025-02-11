a4d534c60d4288d1d7e707e1d23e5e7fabb9d6a2
Leonardo Franca Paulelli <90411076+Leonardo-FP@users.noreply.github.com>
2025-02-10 23:07:37 -0300

    debounce

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,41 +3,17 @@
 
   <router-view></router-view> 
 
-  <img src="https://picsum.photos/200" alt="" id="img" ref="img">
-
-  <ul>
-    <li v-for="(product, index) in products" :key="index" id="product" ref="product">{{ product.name }}</li>
-  </ul>
+  <input type="text" placeholder="search" @keyup="search">
 </template>
 
 <script setup>
   import Nav from '@/components/Nav.vue'
-  import CountChild from './components/CountChild.vue';
-  import {ref, onMounted, reactive} from 'vue';
+  import _ from 'lodash';
 
-  const count = ref(0);
-
-  const img = ref(null);
-  const product = ref([]);
-
-  const products = reactive([
-    {
-      id:1,
-      name:'Imac'
-    },
-    {
-      id:2,
-      name:'Iphone'
-    }
-  ])
-
-  onMounted(() => {
-    // console.log(document.querySelector('#img'));
-    // console.log(document.querySelectorAll('#product'));
-    console.log(product.value);
-  })
+  const search = _.debounce(() => {
+    console.log('search');
+  }, 1000)
 </script>
-
 
 <style scoped>
   
