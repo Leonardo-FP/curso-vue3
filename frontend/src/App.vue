c76e5d64c9c49116ba067356bf18003a0a5de15e
Leonardo Franca Paulelli <90411076+Leonardo-FP@users.noreply.github.com>
2025-02-10 22:51:49 -0300

    tempate ref

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,41 @@
 <template>
   <Nav />
 
-  <router-view></router-view>
+  <router-view></router-view> 
+
+  <img src="https://picsum.photos/200" alt="" id="img" ref="img">
+
+  <ul>
+    <li v-for="(product, index) in products" :key="index" id="product" ref="product">{{ product.name }}</li>
+  </ul>
 </template>
 
 <script setup>
   import Nav from '@/components/Nav.vue'
   import CountChild from './components/CountChild.vue';
-  import {ref} from 'vue';
+  import {ref, onMounted, reactive} from 'vue';
 
   const count = ref(0);
 
-  function add(value){
-    count.value+=value;
-  }
+  const img = ref(null);
+  const product = ref([]);
+
+  const products = reactive([
+    {
+      id:1,
+      name:'Imac'
+    },
+    {
+      id:2,
+      name:'Iphone'
+    }
+  ])
+
+  onMounted(() => {
+    // console.log(document.querySelector('#img'));
+    // console.log(document.querySelectorAll('#product'));
+    console.log(product.value);
+  })
 </script>
 
 
