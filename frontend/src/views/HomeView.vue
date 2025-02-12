6c8874c8f10bfaf0586172cf8296a9ffe7553066
Leonardo Franca Paulelli <90411076+Leonardo-FP@users.noreply.github.com>
2025-02-11 22:03:57 -0300

    paginação

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,8 +3,13 @@
   <input type="text" name="" id="" placeholder="Search" v-model="userSearch" @keyup="search">
 
   <ul>
-    <li v-for="(user, index) in users['users']">{{ user.firstName }} {{ user.lastName }}</li>
+    <li v-for="(user, index) in users['users'].data">{{ user.firstName }} {{ user.lastName }}</li>
   </ul>
+
+  <Bootstrap5Pagination 
+    :data="users['users']"
+    @pagination-change-page="getUsers" 
+  />
 
   <div v-html="userNotFound"></div>
 
@@ -15,18 +20,17 @@
   import http from '@/services/http.js';
   import { onMounted, ref, reactive, computed } from 'vue';
   import _ from 'lodash';
+  import { Bootstrap5Pagination } from 'laravel-vue-pagination';
 
   const users = reactive({users:[]});
   const userSearch = ref();
   const loading = ref(true);
-  const userNotFound = computed(() => {
-    return (!loading.value && users['users'].length <= 0) ? '<span id="notFound">Nenhum user encontrado</span>' : ''
-  })
 
-  onMounted(async () => {
+
+  async function getUsers(page = 1) {
     try{
 
-      const {data} = await http.get('/api/users');
+      const {data} = await http.get('/api/users?page='+Number(page));
       users['users'] = data;
 
     } catch(error) {
@@ -34,6 +38,14 @@
       console.log(error.response.data)
 
     }
+  }
+
+  const userNotFound = computed(() => {
+    return (!loading.value && users['users'].length <= 0) ? '<span id="notFound">Nenhum user encontrado</span>' : ''
+  })
+
+  onMounted(() => {
+    getUsers();
   })
 
   const search = _.debounce(async () => {
