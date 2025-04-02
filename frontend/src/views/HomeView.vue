d32d2bb726ba8c534cd5da3025641c9c7b084622
Leonardo Franca Paulelli <90411076+Leonardo-FP@users.noreply.github.com>
2025-04-01 22:10:52 -0300

    watch

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,95 +1,86 @@
 <template>
+  <input type="text" v-model="user.firstName" placeholder="First Name">
+  <input type="text" v-model="user.lastName" placeholder="Last Name">
 
-  <input type="text" placeholder="Search" @keyup="search" v-model="searchInput">
+  <hr>
 
   <ul>
-    <li v-for="(user, index) in users.data" :key="index">{{ user.firstName }} {{ user.lastName }}</li>
+    <li v-for="(item, index) in items" :key="index">{{ item.name }}
+      <input type="text" v-model="item.price">
+    </li>
   </ul>
-
-  <Bootstrap5Pagination  
-    :data="users" 
-    @pagination-change-page="handlePagination" 
-    :limit="5"
-    align="center"
-  />
-
-  <div v-html="userNotFound"></div>
-
 </template>
 
 <script>
-  import http from '@/services/http.js';
-  import _ from 'lodash';
-  import {Bootstrap5Pagination  } from 'laravel-vue-pagination';
 
   export default {
-
-    components:{Bootstrap5Pagination },
-    
     data(){
       return {
-        users:[],
-        loading:true,
-        searchInput:'',
-      }
-    },
-
-    computed:{
-      userNotFound(){
-        return (!this.loading && this.users.data.length <=0) ? '<span id="notFound">Nenhum user encontrado</span>' : ''
-      }
-    },
-    
-    mounted(){
-      this.getUsers();
-    },
-
-    methods:{
-
-      handlePagination(page){
-        return this.searchInput ? this.searchUsers(page) : this.getUsers(page);
-      },
-      
-      async getUsers(page = 1){
-        try{
-          const {data} = await http.get('/api/users?page='+Number(page));
-          this.loading = false;
-          this.users = data;
-        }catch(error){
-          console.log(error.response.data);
-        }
-      },
-
-      async searchUsers(page = 1){
-        try{
-          const {data} = await http.get('/api/users/search?page='+Number(page),{
-            params:{
-              user: this.searchInput
+        firstName:'',
+        lastName:'',
+        user:{
+          firstName:'',
+          lastName:'',
+          address:{
+            street:''
+          }         
+        },
+        items:[
+            {
+              id:1, 
+              name:'teste1',
+              price:10
+            },
+            {
+              id:2, 
+              name:'teste2',
+              price:100
             }
-          })
+          ]
+      }
+    },
 
-          if(!this.searchInput){
-            this.getUsers();
-          }else{
-            this.users = data;
-          }
+    watch:{
+      // firstName(value, oldValue){
+      //   console.log('new =>'+value, 'old=>'+oldValue)
+      // },
 
-          this.users = data;
-        }catch(error){
-          console.log(error.response.data);
-        }
-      },
-      
-      search:_.debounce(async function (event) {
-        this.searchUsers();
-      }, 1000)
+      // lastName(value, oldValue){
+      //   console.log('new =>'+value, 'old=>'+oldValue)
+      // },
+
+      // 'user.firstName': function(value, oldValue){
+      //   console.log('new =>'+value, 'old=>'+oldValue)
+      // },
+
+      // 'user.lastName': function(value, oldValue){
+      //   console.log('new =>'+value, 'old=>'+oldValue)
+      // },
+
+      // 'user.address.street': function(value, oldValue){
+      //   console.log('new =>'+value, 'old=>'+oldValue)
+      // }
+    },
+
+    watch:{
+      items:{
+        handler(itemsObject, oldValue){
+          itemsObject.forEach(element => {
+            if(!Number.isInteger(Number(element.price))){
+              console.log('Não é um integer '+element.price)
+            }
+          });
+        },
+
+        deep:true,
+        immediate:true
+      }
     }
+    
   }
 </script>
 
 <style >
-  #notFound {
-    color: red;
-  }
+ 
 </style>
 
