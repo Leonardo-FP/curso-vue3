ed690fda711de87d963ea04b8508c0ca27e72fe7
Leonardo-FP <leopaulelli@gmail.com>
2025-04-02 14:57:44 -0300

    watch

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,89 +1,47 @@
 <template>
+  <input type="text" v-model="user.firstName" placeholder="First Name">
+  <input type="text" v-model="user.lastName" placeholder="Last Name">
 
-  <input type="text" name="" id="" placeholder="Search" v-model="userSearch" @keyup="search">
+  <hr>
 
   <ul>
-    <li v-for="(user, index) in users['users'].data">{{ user.firstName }} {{ user.lastName }}</li>
+    <li v-for="item in items" :key="item.id">
+      {{ item.name }}
+      <input type="text" v-model="item.name">
+    </li>
   </ul>
-
-  <Bootstrap5Pagination 
-    :data="users['users']"
-    :limit="2"
-    :show-disabled="true"
-    size="small"
-    align="center"
-    @pagination-change-page="handleEventPagination" 
-  />
-
-  <div v-html="userNotFound"></div>
-
 </template>
 
 <script setup>
+  import {ref, watch, reactive} from 'vue';
 
-  import http from '@/services/http.js';
-  import { onMounted, ref, reactive, computed } from 'vue';
-  import _ from 'lodash';
-  import { Bootstrap5Pagination } from 'laravel-vue-pagination';
+  const firstName = ref('');
+  const lastName = ref('');
 
-  const users = reactive({users:[]});
-  const userSearch = ref();
-  const loading = ref(true);
+  const user = reactive({
+    firstName:'',
+    lastName:'',
+  })
 
-  function handleEventPagination(page){
-    return userSearch.value ? searchUSer(page) : getUsers(page)
-  }
-
-  async function getUsers(page = 1) {
-    try{
-
-      const {data} = await http.get('/api/users?page='+Number(page));
-      users['users'] = data;
-      loading.value = false;
-
-    } catch(error) {
-
-      console.log(error.response.data)
-
+  const items = reactive([
+    {
+      id:1,
+      name:'Leonardo'
+    },
+    {
+      id:2,
+      name:'Paulelli'
     }
-  }
+  ])
 
-  const userNotFound = computed(() => {
-    return (!loading.value && users['users'].data.length <= 0) ? '<span id="notFound">Nenhum user encontrado</span>' : ''
-  })
-
-  onMounted(() => {
-    getUsers();
-  })
-
-  async function searchUSer(page=1){
-    try{
-      const {data} = await http.get('/api/users/search?page='+Number(page),{
-        params:{
-          user:userSearch.value
+  watch(
+    items, (value) => {
+      value.forEach((item) => {
+        if(!Number.isInteger(Number(item.name))){
+          console.log('Not a number'+item.name);
         }
       })
-
-      if(!userSearch.value){
-        getUsers();
-        return;
-      }
-        
-      users['users'] = data;
-
-      } catch(error) {
-      console.log(error.response.data);
-      }
-  }
-
-  const search = _.debounce(async () => {
-    searchUSer();    
-  },1000)
+    }
+  )
 
 </script>
-
-<style>
-  #notFound {
-    color: red;
-  }
-</style>
