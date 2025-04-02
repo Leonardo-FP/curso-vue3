a5eb2e6de595a68f76c2d5d1637753e928151e5a
Leonardo-FP <leopaulelli@gmail.com>
2025-04-02 17:22:25 -0300

    next tick

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,59 +1,24 @@
 <template>
-
-  <span ref="span">Conteúdo do span</span>
-
-  <input type="text" v-model="user.firstName">
+  <button @click="showSomething()">Show</button>
   
-  <ul>
-    <li v-for="item in items">
-      {{ item.name }}
-      <input type="text" v-model="item.name">
-    </li>
-  </ul>
+  <input type="text" v-if="showElement" ref="inputElement">
 </template>
 
 <script setup>
-  import { ref, computed, onMounted, watch, watchEffect, reactive } from 'vue';
- 
-  const firstName = ref('');
-  const lastName = ref('');
-  const span = ref(null);
-  const user = reactive({
-    firstName:'',
-    lastName:''
-  })
+  import { ref, nextTick } from 'vue';
+  
+  const showElement = ref(false);
+  const inputElement = ref(null);
 
-  const items = reactive([
-    {
-      id: 1, 
-      name: 'Leonardo'
-    },
-    {
-      id: 2, 
-      name: 'Paulelli'
-    }
-  ])
+  async function showSomething(){
+    showElement.value = true;
 
-  onMounted(() => {
-    console.log(span.value.textContent);
-  })
+    // Serve para ter acesso ao DOM atualizado
+    await nextTick(() => {
+      inputElement.value.focus();
+      console.log('focus')
+    })
 
-  // Chamado depois do component ser montado
-  // Usado para ter controle das propriedades reativas
-  // Permite obter o valor anterior 
-  // Só é chamado quando a propriedade reativa é alterada
-  watch(
-    () => user.firstName, 
-    (value, oldValue) => {
-      console.log(value, oldValue)
-    }
-  )
-
-  // Chamado antes do component ser montado
-  // Observa tudo
-  // Não permite obter o valor anterior
-  watchEffect(() => {
-    // console.log(span.value.textContent)
-  })
-
+    console.log('carregou')
+  }
 </script>
