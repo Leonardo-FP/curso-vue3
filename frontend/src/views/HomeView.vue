f2a772a87f76fda2579f727ad4316544f8f9b579
Leonardo-FP <leopaulelli@gmail.com>
2025-04-02 15:54:19 -0300

    sistema de conversão de moedas

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,47 +1,51 @@
 <template>
-  <input type="text" v-model="user.firstName" placeholder="First Name">
-  <input type="text" v-model="user.lastName" placeholder="Last Name">
+  <Currency type="text" v-model="dolar" placeholder="Dólar" :options="{ currency: 'USD'} " />
 
   <hr>
 
   <ul>
-    <li v-for="item in items" :key="item.id">
-      {{ item.name }}
-      <input type="text" v-model="item.name">
-    </li>
+    <li>{{ dolarTodayValue }}</li>
+    <li>{{ dolarToReaisValue }}</li>
   </ul>
 </template>
 
 <script setup>
-  import {ref, watch, reactive} from 'vue';
+  import { ref, computed, onMounted, watch } from 'vue';
+  import http from '../services/http';
+  import format from '../services/format';
+  import Currency from '@/components/Currency.vue'
+  
+  const dolar = ref(0);
+  const dolarToday = ref(0);
+  const dolarToReal = ref(0);
 
-  const firstName = ref('');
-  const lastName = ref('');
+  const dolarToReaisValue = computed(() => {
+    return `O valor em reais de ${format(dolar.value, 'en-US', 'USD')} é ${format(dolarToReal.value, 'pt-BR', 'BRL')}`;
+  });
 
-  const user = reactive({
-    firstName:'',
-    lastName:'',
+  const dolarTodayValue = computed(() => {
+    return `O dólar hoje está em: ${format(dolarToday.value, 'pt-BR', 'BRL')}`;
   })
 
-  const items = reactive([
-    {
-      id:1,
-      name:'Leonardo'
-    },
-    {
-      id:2,
-      name:'Paulelli'
+  onMounted(async () => {
+    try{
+      const dolar = await getDolar();
+      dolarToday.value = dolar.high;
+    }catch(error){
+      console.log(error);
     }
-  ])
+    getDolar()
+  })
 
-  watch(
-    items, (value) => {
-      value.forEach((item) => {
-        if(!Number.isInteger(Number(item.name))){
-          console.log('Not a number'+item.name);
-        }
-      })
-    }
-  )
+  async function getDolar(typeCurrency = 'USD-BRL'){
+    const {data} = await http.get('https://economia.awesomeapi.com.br/json/last/'+typeCurrency);
+
+    const currency = typeCurrency.split('-').join('');
+    return data[currency];
+  }
+
+  watch(dolar, (value) => {
+    dolarToReal.value = value * Number(dolarToday.value);
+  })
 
 </script>
